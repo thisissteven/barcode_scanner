ff58af442a2f72de4f0ad769c72caf991e3d7ddd
Steven <ssteven075@gmail.com>
2023-11-03 16:00:45 +0700

    temp

--- lib/home.dart
+++ lib/home.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String barcode = "";
+  List<String> barcode = ["", "", "", ""];
 
   @override
   Widget build(BuildContext context) {
@@ -24,74 +24,140 @@ class _HomePageState extends State<HomePage> {
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Barcode",
-                      style: TextStyle(
-                        color: contentPrimary,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    SvgPicture.asset(
-                      "assets/asterisk.svg",
-                      width: 14,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                RawMaterialButton(
-                  onPressed: () async {
-                    String? value = await Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const QRViewDialog(),
-                      ),
-                    );
-
-                    setState(() {
-                      barcode = value ?? "";
-                    });
-                  },
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6),
-                    side: const BorderSide(
-                      color: borderPrimary,
-                    ),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 10,
-                  ),
+            ...List.generate(
+              barcode.length,
+              (index) {
+                bool isFirstIndex = index == 0;
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
                   child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
                       Expanded(
-                        child: Text(
-                          barcode,
-                          style: const TextStyle(
-                            color: contentPrimary,
-                          ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  "Barcode",
+                                  style: TextStyle(
+                                    color: contentPrimary,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 4,
+                                ),
+                                SvgPicture.asset(
+                                  "assets/asterisk.svg",
+                                  width: 14,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            RawMaterialButton(
+                              onPressed: () async {
+                                String? value =
+                                    await Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => const QRViewDialog(),
+                                  ),
+                                );
+
+                                setState(() {
+                                  barcode[index] = value ?? "";
+                                });
+                              },
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6),
+                                side: const BorderSide(
+                                  color: borderPrimary,
+                                ),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 10,
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      barcode[index],
+                                      style: const TextStyle(
+                                        color: contentPrimary,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  SvgPicture.asset(
+                                    "assets/barcode-scan.svg",
+                                    width: 24,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      SvgPicture.asset(
-                        "assets/barcode-scan.svg",
-                        width: 24,
-                      ),
+                      if (!isFirstIndex)
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: SizedBox(
+                            width: 48,
+                            child: RawMaterialButton(
+                              onPressed: () {
+                                setState(() {
+                                  barcode.removeAt(index);
+                                });
+                              },
+                              padding: const EdgeInsets.all(10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6),
+                                side: const BorderSide(
+                                  color: borderPrimary,
+                                  width: 0.6,
+                                ),
+                              ),
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
+                        ),
                     ],
                   ),
+                );
+              },
+            ),
+            const SizedBox(height: 12),
+            SizedBox(
+              width: double.infinity,
+              child: RawMaterialButton(
+                onPressed: () {
+                  setState(() {
+                    barcode.add("");
+                  });
+                },
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
                 ),
-              ],
-            )
+                fillColor: Colors.teal.shade600,
+                padding: const EdgeInsets.all(12),
+                child: const Text(
+                  "Tambah",
+                  style: TextStyle(
+                    color: borderPrimary,
+                  ),
+                ),
+              ),
+            ),
           ]),
         ),
       ),
